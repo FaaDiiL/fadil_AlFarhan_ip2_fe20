f33b0579e60ca3993d343e3f6f23e8e4bff3cd68
Fadil Al <fadil.alfarhan@gmail.com>
2021-02-26 07:14:47 +0100

    Form functionality added.

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-
 const Wrapper = styled.main`
   position: relative;
   width: 100%;
@@ -17,41 +17,42 @@ const Wrapper = styled.main`
 `
 
 function Contact() {
+  const [myFormVal, setMyFormVal] = useState([{ fullName:'Fadil Al', email: 'YourEmail@mail.com', message: 'Your message here'}])
+  const [fullName, setFullName] = useState('Fadil')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  console.log(myFormVal)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setMyFormVal([{fullName: fullName,email:email,message:message}])
+    console.log(e.target.form)
+  }
   return (
     <Wrapper id='Contact'>
-      <form
-        action='/#'
+      <form 
         className='w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin'
+        onSubmit={handleSubmit}
       >
         <h2 className='w3-center'>Contact Us</h2>
+        {/** Full Name */}
         <div className='w3-row w3-section'>
-          <div className='w3-col' style={{ width: 150 }}>
+          <div className='w3-col'>
             <i className='w3-xxlarge fa fa-user' />
           </div>
           <div className='w3-rest'>
             <input
               className='w3-input w3-border'
-              name='first'
+              name='FullName'
               type='text'
-              placeholder='First Name'
+              placeholder='John Doe'
+              onChange={(e)=> setFullName(e.target.value) + console.log(e.target.value)}
             />
           </div>
         </div>
+        {/** Email */}
         <div className='w3-row w3-section'>
-          <div className='w3-col' style={{ width: 150 }}>
-            <i className='w3-xxlarge fa fa-user' />
-          </div>
-          <div className='w3-rest'>
-            <input
-              className='w3-input w3-border'
-              name='last'
-              type='text'
-              placeholder='Last Name'
-            />
-          </div>
-        </div>
-        <div className='w3-row w3-section'>
-          <div className='w3-col' style={{ width: 150 }}>
+          <div className='w3-col'>
             <i className='w3-xxlarge fa fa-envelope-o' />
           </div>
           <div className='w3-rest'>
@@ -59,40 +60,35 @@ function Contact() {
               className='w3-input w3-border'
               name='email'
               type='text'
-              placeholder='Email'
+              placeholder='JohnDoe@mail.com'
+              onChange={(e)=> setEmail(e.target.value) + console.log(e.target.value)}
+
             />
           </div>
         </div>
-        <div className='w3-row w3-section'>
-          <div className='w3-col' style={{ width: 50 }}>
-            <i className='w3-xxlarge fa fa-phone' />
-          </div>
-          <div className='w3-rest'>
-            <input
-              className='w3-input w3-border'
-              name='phone'
-              type='text'
-              placeholder='Phone'
-            />
-          </div>
-        </div>
+        {/** Message */}
         <div className='w3-row w3-section'>
           <div className='w3-col' style={{ width: 50 }}>
             <i className='w3-xxlarge fa fa-pencil' />
           </div>
           <div className='w3-rest'>
-            <input
+            <textarea 
               className='w3-input w3-border'
-              name='message'
-              type='text'
-              placeholder='Message'
-            />
+              name="message"
+          rows="5" cols="33"
+          placeholder='Hi! I want to offer you a job. Are you interested?'
+          onChange={(e)=> setMessage(e.target.value) + console.log(e.target.value)}>
+          
+          </textarea>
           </div>
         </div>
         <button className='w3-button w3-block w3-section w3-blue w3-ripple w3-padding'>
           Send
         </button>
       </form>
+      <h3>{myFormVal[0].fullName}</h3>
+      <h3>{myFormVal[0].email}</h3>
+      <h3>{myFormVal[0].message}</h3>
     </Wrapper>
   )
 }
